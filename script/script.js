ce734bddecd7ac2a977e392349ae0a8ce8b20f4a
Shcherbinin Nikolay <13_demon_89@mail.ru>
2021-09-07 20:21:18 +0300

    Практика #2

--- script/script.js
+++ script/script.js
@@ -6,12 +6,30 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for(let i=0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.lenght < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }
+    else{
+        console.log('error');
+        i--;
+    }
+} 
+
+if(personalMovieDB.count < 10){
+    console.log('Меньше 10');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Больше 10 и меньше 30");
+}else if(personalMovieDB.count >=30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
